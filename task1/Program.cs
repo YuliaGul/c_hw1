b60d9bd38981c761431cca6c6331315cdc41e90d
yu_gulyaeva <mangostinshop@mail.ru>
2022-11-25 22:53:25 +0300

    Задача 1: найти большее чило из 2х

---
+++ task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+{
+    Console.Write("Первое число больше: ");
+    Console.WriteLine(number1);
+}
+else
+{
+    Console.Write("Второе число больше: ");
+    Console.WriteLine(number2);
+}
