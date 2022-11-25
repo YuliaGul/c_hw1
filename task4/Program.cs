d426c4e418f3dfa3d613fb72d1f1f8d8bd7e4066
yu_gulyaeva <mangostinshop@mail.ru>
2022-11-25 23:34:40 +0300

    Задача 4: все четные от 1 до N

---
+++ task4/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+Console.WriteLine("Четные числа: ");
+while (index <= n)
+{   if (index % 2 == 0)
+        {
+            Console.WriteLine(index);
+        }
+    index ++;
+}
