2097551b6eb5da4a9dd1e062af2cc348f78150a9
yu_gulyaeva <mangostinshop@mail.ru>
2022-11-25 23:27:11 +0300

    Задача 3: четное или нечетное?

---
+++ task3/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.WriteLine("Число является четным");
+}
+else
+{
+    Console.WriteLine("Число является нечетным");
+}
